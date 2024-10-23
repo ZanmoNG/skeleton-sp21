7c426cdfe241fee0aae0a1d1cdb5fb442493c200
ZanmoNG <3062309690@qq.com>
2024-10-23 22:24:28 +0800

    first time done with proj1 checkpoint

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,87 @@
+package deque;
+
+public class ArrayDeque<T> {
+    T[] items;
+    int headIndex;
+    int size;
+
+    public ArrayDeque(){
+        items = (T[]) new Object[100];
+        size = 0;
+        headIndex = 33;
+    }
+
+    private void resize(int theSize){
+        T[] arr = (T[]) new Object[theSize];
+        int newHeadSize = theSize / 3;
+        System.arraycopy(items, headIndex, arr, newHeadSize, size);
+        items = arr;
+    }
+
+    public void addFirst(T item){
+        if (headIndex - 1 < 0){
+            resize(size * 2);
+        }
+        items[headIndex-1] = item;
+        size++;
+        headIndex--;
+    }
+
+    public void addLast(T item){
+        if (headIndex + size >= 100){
+            resize(size * 2);
+        }
+        items[headIndex+size] = item;
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        for (int i = headIndex; i < headIndex + size; i++){
+            System.out.print(items[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst(){
+        if (size == 0) {
+            return null;
+        }
+        else {
+            T theItem = items[headIndex];
+            items[headIndex] = null;
+            headIndex++;
+            size--;
+            if (size < items.length / 4){
+                resize(items.length / 2);
+            }
+            return theItem;
+        }
+    }
+
+    public T removeLast(){
+        if (size == 0) {
+            return null;
+        }
+        else {
+            T theItem = items[headIndex + size - 1];
+            items[headIndex + size - 1] = null;
+            size--;
+            if (size < items.length / 4){
+                resize(items.length / 2);
+            }
+            return theItem;
+        }
+    }
+
+    public T get(int index){
+        return items[headIndex + index];
+    }
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,92 @@
+package deque;
+
+public class LinkedListDeque<T> {
+    private class LinkedListNode{
+        LinkedListNode prev;
+        T item;
+        LinkedListNode next;
+
+        public LinkedListNode(LinkedListNode p, T x, LinkedListNode n){
+            prev = p;
+            item = x;
+            next = n;
+        }
+    }
+
+    private LinkedListNode headSentinel;
+    private LinkedListNode tailSentinel;
+    private int size;
+
+    /** constructor */
+    public LinkedListDeque(){
+        headSentinel = new LinkedListNode(null, null, null);
+        tailSentinel = new LinkedListNode(headSentinel, null, null);
+        headSentinel.next = tailSentinel;
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        headSentinel.next = new LinkedListNode(headSentinel, item, headSentinel.next);
+        headSentinel.next.next.prev = headSentinel.next;
+        size++;
+    }
+
+    public void addLast(T item){
+        tailSentinel.prev = new LinkedListNode(tailSentinel.prev, item, tailSentinel);
+        tailSentinel.prev.prev.next = tailSentinel.prev;
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        LinkedListNode p = headSentinel.next;
+        while (p != tailSentinel){
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public T removeLast(){
+        if (size == 0)
+            return null;
+        else {
+            T theItem = tailSentinel.prev.item;
+            tailSentinel.prev = tailSentinel.prev.prev;
+            tailSentinel.prev.next = tailSentinel;
+            size--;
+            return theItem;
+        }
+    }
+
+    public T removeFirst(){
+        if (size == 0)
+            return null;
+        else {
+            T theItem = headSentinel.next.item;
+            headSentinel.next = headSentinel.next.next;
+            headSentinel.next.prev = headSentinel;
+            size--;
+            return theItem;
+        }
+    }
+
+    public T get(int index){
+        LinkedListNode p = headSentinel.next;
+        int i;
+        for (i = 0; i < size && i < index; i++){
+            p = p.next;
+        }
+        if (i == size)
+            return null;
+        else
+            return p.item;
+    }
+}
